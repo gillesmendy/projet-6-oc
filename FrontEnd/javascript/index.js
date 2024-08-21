c35fce7ee373b5e4778060cf566185a3ce3a9088
gillesmendy <165325415+gillesmendy@users.noreply.github.com>
2024-08-21 12:07:53 +0200

    feature: Projects added to the gallery

--- FrontEnd/javascript/index.js
+++ FrontEnd/javascript/index.js
@@ -1,4 +1,4 @@
-//Récupération des projets avec fetch
+// Récupération des projets avec fetch
 
 fetch('http://localhost:5678/api/projects')
   .then(response => {
@@ -15,3 +15,26 @@ fetch('http://localhost:5678/api/projects')
   .catch(error => {
     console.error('Erreur lors de la récupération des projets:', error);
   });
+
+
+  // Ajouter les projets à la gallerie 
+
+  function displayProjects(projects) {
+    const gallery = document.querySelector('.gallery'); 
+  
+    projects.forEach(project => {
+      const projectElement = document.createElement('div');
+      projectElement.classList.add('project'); 
+  
+      const image = document.createElement('img');
+      image.src = project.imageUrl; 
+      image.alt = project.title;  
+  
+      const title = document.createElement('h3');
+      title.textContent = project.title;
+  
+      projectElement.appendChild(image);
+      projectElement.appendChild(title);
+      gallery.appendChild(projectElement);
+    });
+  }
