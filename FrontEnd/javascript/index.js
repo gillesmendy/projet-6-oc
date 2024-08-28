dbbea48e35d5821a4eae553f9caf550c8a37fe9c
gillesmendy <165325415+gillesmendy@users.noreply.github.com>
2024-08-28 10:46:21 +0200

    feature: function created to add the categories to the filters part

--- FrontEnd/javascript/index.js
+++ FrontEnd/javascript/index.js
@@ -43,3 +43,25 @@ async function getCategories() {
     const response = await fetch('http://localhost:5678/api/categories');
     return await response.json();
 }
+
+// Ajouter les categories à la div filters
+function addCategories(filter) {
+    const filters = document.querySelector('.filters'); // Sélectionne l'élément avec la classe 'filters'
+  
+    const filterElement = document.createElement('div'); // Crée un nouvel élément 'div' pour chaque catégorie
+    filterElement.classList.add('filter'); // Ajoute la classe 'filter' pour le style
+  
+    filterElement.textContent = filter.name; // Ajoute le nom de la catégorie comme contenu textuel
+  
+    filters.appendChild(filterElement); // Ajoute cet élément à l'élément '.filters'
+}
+
+//Combiner les deux fonctions
+async function showCategories() {
+    const categories = await getCategories(); // Récupère les catégories depuis l'API
+    categories.forEach((category) => { // Parcourt chaque catégorie
+        addCategories(category); // Ajoute chaque catégorie à la page
+    });
+}
+
+showCategories(); // Appelle la fonction pour afficher les catégories
